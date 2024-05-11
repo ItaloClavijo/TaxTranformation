cd2ba9cc52450cb7224cd30eba487fba4bfe4d9e
Italo Sebastián Clavijo Curi <106178559+ItaloClavijo@users.noreply.github.com>
2024-05-11 16:53:34 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path:'tt/news',
+        path:'',
         title: 'Admin Libros',
         loadComponent: () => import('./pages/top-head-liners/top-head-liners.component')
     },
